cabfa143729e3530bb46a151473d15a85e45edb5
Victor <victorelezua@gmail.com>
2026-04-04 12:35:18 +0100

    Initial Commit

---
+++ TaskTracker/TaskTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskTrackerApp.swift
+//  TaskTracker
+//
+//  Created by Victor on 4/4/26.
+//
+
+import SwiftUI
+
+@main
+struct TaskTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
